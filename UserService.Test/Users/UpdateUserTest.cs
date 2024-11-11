b0ac2630aba574d3d176314bcaaaecd282f37287
Caonabo Mena <cmena7512@gmail.com>
2024-11-11 15:55:46 -0400

    feat[UserService]: add update user endpoints and test

---
+++ UserService.Test/Users/UpdateUserTest.cs
@@ -0,0 +1,175 @@
+﻿using Bogus;
+using FluentAssertions;
+using FluentValidation.TestHelper;
+using UserService.Entities;
+using UserService.Errors;
+using UserService.Features.Users;
+using UserService.Shared;
+
+namespace UserService.Test.Users
+{
+    public class UpdateUserTest : BaseTest
+    {
+        [Fact]
+        public async Task UpdateUser_WithName_ShouldUpdateNameOnly()
+        {
+            // arrange
+            var user = await CreateUser();
+
+            var command = new UserUpdate.Command
+            {
+                Id =  user.Id.Value,
+                Name = "Ramon",
+            };
+
+            //act
+            var result = await sender.Send(command);
+            result.IsSuccess.Should().BeTrue();
+            result.IsFailed.Should().BeFalse();
+
+            //assert
+            var updatedUser = await context.Users.FindAsync(user.Id);
+            updatedUser!.Name.Should().Be(command.Name);
+        }
+
+        [Fact]
+        public async Task UpdateUser_WithRole_ShouldUpdateRoleOnly()
+        {
+            // arrange
+            var user = await CreateUser();
+
+            var command = new UserUpdate.Command
+            {
+                Id = user.Id.Value,
+                Role = Role.Admin
+            };
+
+            //act
+            var result = await sender.Send(command);
+            result.IsSuccess.Should().BeTrue();
+            result.IsFailed.Should().BeFalse();
+
+            //assert
+            var updatedUser = await context.Users.FindAsync(user.Id);
+            updatedUser!.Role.Should().Be(command.Role);
+        }
+
+        [Theory]
+        [InlineData("calle los llanos #21", "los rios", "santo domingo", "republica dominicana")]
+        [InlineData("calle san francisco", null, "Santiago", "republica dominicana")]
+        [InlineData("calle ramon matias mella", null, null, null)]
+        [InlineData(null, null, null, null)]
+        [InlineData(null, null, "NY", "US")]
+        [InlineData(null, null, "Miami", null)]
+        public async Task UpdateUser_WithAddress_ShouldUpdateAddressOnly(string street, string sector, string city, string country)
+        {
+            // arrange
+            var user = await CreateUser();
+
+            var command = new UserUpdate.Command
+            {
+                Id = user.Id.Value,
+                Address = new Address(street, sector, city, country)
+            };
+
+            //act
+            var result = await sender.Send(command);
+            result.IsSuccess.Should().BeTrue();
+            result.IsFailed.Should().BeFalse();
+
+            //assert
+            var updatedUser = await context.Users.FindAsync(user.Id);
+
+            if (command.Address.Street != null)
+                updatedUser!.Address.Street.Should().BeEquivalentTo(command.Address.Street);
+
+            if (command.Address.Sector != null)
+                updatedUser!.Address.Sector.Should().BeEquivalentTo(command.Address.Sector);
+
+            if (command.Address.Country != null)
+                updatedUser!.Address.Country.Should().BeEquivalentTo(command.Address.Country);
+
+            if (command.Address.City != null)
+                updatedUser!.Address.City.Should().BeEquivalentTo(command.Address.City);
+
+
+            updatedUser!.Address.Should().NotBeNull();
+            updatedUser!.Address.Street.Should().NotBeNull();
+            updatedUser!.Address.Sector.Should().NotBeNull();
+            updatedUser!.Address.City.Should().NotBeNull();
+            updatedUser!.Address.Country.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task UpdateUser_WithoutId_ShouldReturnFailure()
+        {
+            // arrange
+            var user = await CreateUser();
+
+            var command = new UserUpdate.Command
+            {
+                Name = "Ramon",
+                Role = Role.Admin,
+            };
+
+            //act
+            var result = await sender.Send(command);
+
+
+            //assert
+            result.IsSuccess.Should().BeFalse();
+            result.IsFailed.Should().BeTrue();
+            var validator = new UserUpdate.Validator();
+            var validatorResult = await validator.TestValidateAsync(command);
+            validatorResult.ShouldHaveValidationErrorFor(x => x.Id);
+        }
+
+        [Fact]
+        public async Task UpdateUser_WithNonExistingId_ShouldReturnFailure()
+        {
+            // arrange
+            var userId = Guid.NewGuid();
+            var command = new UserUpdate.Command
+            {
+                Id = userId,
+                Name = "Ramon",
+                Role = Role.Admin,
+            };
+
+            //act
+            var result = await sender.Send(command);
+
+
+            //assert
+            result.IsSuccess.Should().BeFalse();
+            result.IsFailed.Should().BeTrue();
+            result.HasError<ApplicationError>().Should().BeTrue();
+            result.Errors.Should().ContainEquivalentOf(UserErrorMessage.UserNotFound(new UserId(userId)));
+        }
+
+        private async Task<User> CreateUser()
+        {
+            Randomizer.Seed = new Random(10);
+            var password = Password.Create("very_strong_password.");
+            var user = new Faker<User>().CustomInstantiator(f =>
+                User.Create(
+                     f.Name.FullName(),
+                new Email(f.Internet.Email()),
+                password.Value,
+                new Address(
+                    f.Address.StreetName(),
+                    f.Address.SecondaryAddress(),
+                    f.Address.City(), f.Address.Country()
+                    )
+                ).Value
+            ).Generate();
+
+
+
+            context.Add(user);
+            await context.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}
---
+++ UserService/Features/Users/UserUpdate.cs
@@ -0,0 +1,79 @@
+﻿using FluentResults;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using UserService.Abstractions;
+using UserService.Database;
+using UserService.Entities;
+using UserService.Shared;
+
+namespace UserService.Features.Users
+{
+    public class UserUpdate
+    {
+        public record Command : ICommand
+        {
+            public Guid Id { get; set; }
+            public string? Name { get; set; }
+            public Role? Role { get; set; }
+            public Address? Address { get; set; }
+        }
+
+        public sealed class Handler(ApplicationContext context) : ICommandHandler<Command>
+        {
+            public async Task<Result> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var userId = new UserId(request.Id);
+                var user = await context.Users.FirstOrDefaultAsync(x => x.Id.Equals(userId), cancellationToken: cancellationToken);
+
+                if (user == null)
+                    return Result.Fail(UserErrorMessage.UserNotFound(userId));
+
+                user.SetName(request.Name);
+                user.SetRole(request.Role);
+                user.SetAddress(request.Address);
+
+                context.Update(user);
+
+                await context.SaveChangesAsync(cancellationToken);
+
+                return Result.Ok();
+
+            }
+        }
+
+        public sealed class Validator : AbstractValidator<Command>
+        {
+            public Validator()
+            {
+                RuleFor(x => x.Id).NotEmpty();
+                RuleFor(x => x.Name).NotEmpty().When(x => x.Name != null);
+                RuleFor(x => x.Address).SetValidator(new AddressValidator()!).When(x => x.Address != null);
+            }
+        }
+
+        public class AddressValidator : AbstractValidator<Address>
+        {
+            public AddressValidator()
+            {
+                RuleFor(x => x.Street).NotEmpty().When(x => x.Street != null);
+                RuleFor(x => x.Sector).NotEmpty().When(x => x.Sector != null);
+                RuleFor(x => x.City).NotEmpty().When(x => x.City != null);
+                RuleFor(x => x.Country).NotEmpty().When(x => x.Country != null);
+            }
+        }
+
+        public sealed class Endpoint : IEndpoint
+        {
+            public void MapEndpoint(IEndpointRouteBuilder app)
+            {
+                app.MapPatch("users/{id:guid}", async (Guid id, Command command, ISender sender) =>
+                {
+                    command.Id = id;
+                    var result = await sender.Send(command);
+                    return Results.Ok();
+                });
+            }
+        }
+    }
+}
