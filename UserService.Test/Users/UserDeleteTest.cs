9b6ffd8a3d06c9ec68ca3144c0b686f2e4ed216d
Caonabo Mena <cmena7512@gmail.com>
2024-11-11 15:56:23 -0400

    feat[UserService:UserDelete]: add user delete endpoint and tests

---
+++ UserService.Test/Users/UserDeleteTest.cs
@@ -0,0 +1,70 @@
+﻿using Bogus;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using UserService.Entities;
+using UserService.Errors;
+using UserService.Features.Users;
+using UserService.Shared;
+
+namespace UserService.Test.Users
+{
+    public class UserDeleteTest : BaseTest
+    {
+        [Fact]
+        public async Task UserDelete_WhenIdIsGiven_ShouldDeleteUser()
+        {
+            //arrange
+            var user = await CreateUser();
+            var command = new UserDelete.Command(user.Id);
+            //act
+            var result = await sender.Send(command);
+
+            //assert
+            result.IsSuccess.Should().BeTrue();
+            result.IsFailed.Should().BeFalse();
+            var isDeleted = !await context.Users.AnyAsync(x => x.Id == user.Id);
+            isDeleted.Should().BeTrue();
+        }
+        [Fact]
+        public async Task UserDelete_WhenIdDontExist_ShoulReturnNotFound()
+        {
+            //arrange
+            var userId = new UserId(Guid.NewGuid());
+            var command = new UserDelete.Command(userId);
+            //act
+            var result = await sender.Send(command);
+
+            //assert
+            result.IsSuccess.Should().BeFalse();
+            result.IsFailed.Should().BeTrue();
+            result.HasError<ApplicationError>().Should().BeTrue();
+            result.Errors.Should().NotBeEmpty();
+            result.Errors.Should().ContainEquivalentOf(UserErrorMessage.UserNotFound(userId));
+        }
+
+        private async Task<User> CreateUser()
+        {
+            Randomizer.Seed = new Random(10);
+            var password = Password.Create("very_strong_password.");
+            var user = new Faker<User>().CustomInstantiator(f =>
+                User.Create(
+                     f.Name.FullName(),
+                new Email(f.Internet.Email()),
+                password.Value,
+                new Address(
+                    f.Address.StreetName(),
+                    f.Address.SecondaryAddress(),
+                    f.Address.City(), f.Address.Country()
+                    )
+                ).Value
+            ).Generate();
+
+
+
+            context.Add(user);
+            await context.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}
---
+++ UserService/Features/Users/UserDelete.cs
@@ -0,0 +1,45 @@
+﻿using FluentResults;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using UserService.Abstractions;
+using UserService.Database;
+using UserService.Entities;
+using UserService.Shared;
+
+namespace UserService.Features.Users
+{
+    public class UserDelete
+    {
+        public record Command(UserId UserId) : ICommand;
+        public sealed class Handler(ApplicationContext context) : ICommandHandler<Command>
+        {
+            public async Task<Result> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var user = await context.Users.FirstOrDefaultAsync(u => u.Id.Equals(request.UserId), cancellationToken: cancellationToken);
+                if (user == null)
+                    return Result.Fail(UserErrorMessage.UserNotFound(request.UserId));
+
+                context.Users.Remove(user);
+
+                await context.SaveChangesAsync(cancellationToken);
+                return Result.Ok();
+            }
+        }
+
+        public sealed class Endpoint : IEndpoint
+        {
+            public void MapEndpoint(IEndpointRouteBuilder app)
+            {
+                app.MapDelete("users/{id:guid}", async (Guid id, ISender sender) =>
+                {
+                    var result = await sender.Send(new Command(new UserId(id)));
+
+                    if (result.IsFailed)
+                        return Results.NotFound();
+
+                    return Results.NoContent();
+                });
+            }
+        }
+    }
+}
